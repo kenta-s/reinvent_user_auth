b3aefcf72f9564cc5bfefc36853da0fbb7121c06
Kenta <shirai.kenta@vareal.co.jp>
2016-08-07 10:30:05 +0900

    User login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get    'sessions/new'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
